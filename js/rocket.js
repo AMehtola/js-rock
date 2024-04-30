542c242eff44d358385672dd07ff7f1c9780c67c
Alexander <143174402+AMehtola@users.noreply.github.com>
2024-04-30 09:19:18 +0300

    a

---
+++ js/rocket.js
@@ -0,0 +1,50 @@
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function(state) {
+	document.body.className = "body-state" + state;
+	clearInterval(timer);
+	countdownNumber = 10;
+	document.getElementById("countdown").innerHTML = countdownNumber;
+
+
+	if (state == 2) {
+		timer = setInterval(function() {
+			countdownNumber = countdownNumber - 1;
+			document.getElementById("countdown").innerHTML = countdownNumber;
+
+			if (countdownNumber > 4 && countdownNumber <= 7) {
+				
+				document.getElementById("liftoff").className = "liftoff show";
+			} else {
+				document.getElementById("liftoff").className = "liftoff";
+			}
+
+			if (countdownNumber > 1 && countdownNumber <= 4) {
+				
+				document.getElementById("blastoff").className = "blastoff show";
+			} else {
+				document.getElementById("blastoff").className = "blastoff";
+			}
+
+			if (countdownNumber <= 0) {
+				changeState(3);
+			}
+		}, 500);
+	} else if (state == 3) {
+		var success = setTimeout(function() {
+			var randomNumber = Math.round(Math.random() * 10);
+
+			console.log("randomNumber:", randomNumber);
+
+		
+			if (randomNumber > 3) {
+				
+				changeState(4);
+			} else {
+			
+				changeState(5);
+			}
+		}, 2000);
+	}
+};
